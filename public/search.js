71e037d5d66c15951818102888249caa9544adc6
Kayce Heap <kayce.heap@gmail.com>
2022-02-06 13:55:33 -0700

    added some css for the movie div added background color and border made all the images the same size

--- public/search.js
+++ public/search.js
@@ -1,4 +1,4 @@
-const searchBtn = document.querySelector("#search");
+const searchBtn = document.querySelector("#search_btn");
 searchBtn.addEventListener("click", function(event) {
     event.preventDefault();
     const searchString = document.querySelector("#search_text");
