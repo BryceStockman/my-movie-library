cc2e253d13049b1516d15b3edcd7afe2e6d283d8
Kayce Heap <kayce.heap@gmail.com>
2022-02-10 21:13:50 -0700

    finished with the handle bars in the search html, its now running on express server

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,20 +1,26 @@
 const router = require('express').Router();
-import fetch from'node-fetch';
+const fetch = require('node-fetch');
 
-router.get('/search',(req, res) => {
-    let apiUrl = `http://www.omdbapi.com/?apikey=5205bbcf&s=home alone`;
+router.get('/search', (req, res) => {
+    if (req.query.title) {
 
-   fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-       console.log(data);
-      if (data.Response === 'False') {
-        alert('Please enter a valid movie title');
-      } else {
-        res.render('homepage', data);
-      }
+    
+    let apiUrl = `http://www.omdbapi.com/?apikey=5205bbcf&s=${req.query.title}`;
+
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+            if (data.Response === 'False') {
+                console.log('Please enter a valid movie title');
+            } else {
+                res.render('homepage', data);
+            }
+        });
     });
-  });
-   
+}else {
+    res.render('homepage', null)
+}
+
 });
 
 module.exports = router;
