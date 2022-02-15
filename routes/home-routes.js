0347bb3391763a34991ceaa8e3cb111476825c80
Kayce Heap <kayce.heap@gmail.com>
2022-02-14 20:12:48 -0700

    working on homepage handlebars

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const fetch = require('node-fetch');
+//const { SequelizeScopeError } = require('sequelize/types');
+const {UserMovie, Movie, User} = require('../models');
 
 router.get('/search', (req, res) => {
     if (req.query.title) {
@@ -23,6 +25,39 @@ router.get('/search', (req, res) => {
 });
 
 router.get('/', (req,res) => {
-    res.render('homepage')
+    // Call movie api and get all movies in library
+    // For each movie, call omdbapi for each title to get the poster
+    UserMovie.findAll({
+        attributes: ['id', 'movie_id', 'user_id', 'movie_shelf'],
+         include: [
+             { 
+                 model: Movie,
+                 attributes: ['id', 'movie_name', 'imdb_id']
+             }
+         ]
+       
+    }).then(async movieData => {
+        movieData = movieData.map(el => el.get({ plain: true }))
+        const toWatch = movieData.filter(movie => movie.movie_shelf === 'to watch');
+        console.log(toWatch);
+        const watched = movieData.filter(movie => movie.movie_shelf === 'watched');
+        console.log(watched);
+
+        // call the api and get the poster for each title
+        for( let i = 0; i < movieData.length; ++i)
+        {
+          let movieUrl = `http://www.omdbapi.com/?apikey=5205bbcf&i=${movieData[i].movie.imdb_id}`;
+          fetch(movieUrl).then(function(response) {
+            response.json().then(function(data){
+                console.log(data);
+                movieData[i].Poster = data.Poster;
+            })
+          })
+        }
+
+        await sleep(2000)
+        console.log(movieData)
+        res.render('homepage', movieData);
+    })    
 })
 module.exports = router;
